8caaad28b4c03398e9412b73efb571de8e2d258b
Techmaster3000 <52240882+Techmaster3000@users.noreply.github.com>
2024-05-25 14:22:27 +0200

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+k
+;src/main/java/com/example/mysticmindfx/HelloController.java,a\6\a6b74f398542417098f066ca2bcea5f673e613cf
+k
+;src/main/resources/com/example/mysticmindfx/hello-view.fxml,2\4\24c35ebd08c2573ff73316efcd7a27b7b4a28ef7
+l
+<src/main/java/com/example/mysticmindfx/HelloApplication.java,5\8\58fdea409e385b7cf1daa7b8f96fd8125c9b06a9
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.mysticmindfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.mysticmindfx to javafx.fxml;
+    exports com.example.mysticmindfx;
+}
