3c4ca98a5cefd1fc86c409652a280fd218108851
23038152 <23038152@student.hhs.nl>
2024-05-30 12:40:23 +0200

    resource Selector (werkt nog niet)

--- src/main/java/com/example/mysticmindfx/HelloApplication.java
+++ src/main/java/com/example/mysticmindfx/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.xml.catalog.CatalogFeatures;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -17,7 +18,5 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+
 }
